671d32f774ef8025c7e93d8509e59f234b2153e0
LeraLukashova <lerashilova07@gmail.com>
2023-05-01 22:14:43 +0300

    Добавлен список веток на main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+// Выполненные задания находятся в соответствующих ветках.
+
+// Список веток:
+// main
+// task64
+// task66
+// task68
+
+
+
